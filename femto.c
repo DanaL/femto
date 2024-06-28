f11d61ad619f49245a250a2b9fc1faa4dc7da830
Dana Larose <ywg.dana@gmail.com>
2024-06-27 22:50:39 -0500

    Initial import, raw mode set up

---
+++ femto.c
@@ -0,0 +1,63 @@
+
+#include <ctype.h>
+#include <errno.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <termios.h>
+#include <unistd.h>
+
+struct termios orig_termios;
+
+void die(const char *s)
+{
+  perror(s);
+  exit(1);
+}
+
+void disable_rawmode(void)
+{
+  if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios) == -1)
+    die("tcsetattr");
+}
+
+void enable_rawmode(void)
+{
+  if (tcgetattr(STDIN_FILENO, &orig_termios) == -1)
+    die("tcgetattr");
+
+  atexit(disable_rawmode);
+
+  struct termios raw;
+
+  tcgetattr(STDIN_FILENO, &raw);
+  raw.c_iflag &= ~(BRKINT | ICRNL | INPCK | ISTRIP | IXON);
+  raw.c_oflag &= ~(OPOST);
+  raw.c_cflag |= (CS8);
+  raw.c_lflag &= ~(ECHO | ICANON | IEXTEN | ISIG);
+  raw.c_cc[VMIN] = 0;
+  raw.c_cc[VTIME] = 1;
+
+  tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
+}
+
+int main(void)
+{
+  enable_rawmode();
+
+  while (1) {
+    char c = '\0';
+    if (read(STDIN_FILENO, &c, 1) == -1 && errno != EAGAIN) 
+      die("read");    
+    if (iscntrl(c)) {
+      printf("%d\r\n", c);
+    }
+    else {
+      printf("%d (%c)\r\n", c, c);
+    }
+
+    if (c == 'q')
+      break;
+  }
+  
+  printf("goodbye.\r\n");
+}
