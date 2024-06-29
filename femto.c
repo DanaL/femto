82acfa229e7b00055a087b0da1041a9874c6e5d9
Dana Larose <ywg.dana@gmail.com>
2024-06-28 23:25:57 -0500

    Some refactoring

--- femto.c
+++ femto.c
@@ -6,10 +6,18 @@
 #include <termios.h>
 #include <unistd.h>
 
+// Defines
+#define CTRL_KEY(k) ((k) & 0x1f)
+
+// Data
 struct termios orig_termios;
 
+// Terminal
 void die(const char *s)
 {
+  write(STDOUT_FILENO, "\x1b[2J", 4);
+  write(STDOUT_FILENO, "\x1b[H", 3);
+
   perror(s);
   exit(1);
 }
@@ -40,23 +48,60 @@ void enable_rawmode(void)
   tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
 }
 
+char editor_read_key(void)
+{
+  int nread;
+  char c;
+  while ((nread = read(STDERR_FILENO, &c, 1)) != 1) {
+    if (nread == -1 && errno != EAGAIN)
+      die("read");
+  }
+
+  return c;
+}
+
+// Output
+
+void editor_draw_rows(void)
+{
+  int y;
+  for (y = 0; y < 24; y++) {
+    write(STDOUT_FILENO, "~\r\n", 3);
+  }
+}
+
+void editor_refresh_screen(void)
+{
+  write(STDERR_FILENO, "\x1b[2J", 4);
+  write(STDERR_FILENO, "\x1b[H", 3);
+
+  editor_draw_rows();
+
+  write(STDERR_FILENO, "\x1b[H", 3);
+}
+
+// Input
+void editor_process_keypress(void)
+{
+  char c = editor_read_key();
+
+  switch (c) {
+    case CTRL_KEY('q'):
+      write(STDOUT_FILENO, "\x1b[2J", 4);
+      write(STDOUT_FILENO, "\x1b[H", 3);
+      exit(0);
+      break;
+  }
+}
+
+// Init
 int main(void)
 {
   enable_rawmode();
 
   while (1) {
-    char c = '\0';
-    if (read(STDIN_FILENO, &c, 1) == -1 && errno != EAGAIN) 
-      die("read");    
-    if (iscntrl(c)) {
-      printf("%d\r\n", c);
-    }
-    else {
-      printf("%d (%c)\r\n", c, c);
-    }
-
-    if (c == 'q')
-      break;
+    editor_refresh_screen();
+    editor_process_keypress();
   }
   
   printf("goodbye.\r\n");
